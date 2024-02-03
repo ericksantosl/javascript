e086e59d68a836a8a78eedd8e5ac27c8026dc269
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-02-03 08:33:01 -0300

    Update ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 101
+console.log(`A velociade do seu carro é ${vel}`)
+if(vel > 100) { // Condição Simples
+    console.log (`Você foi multado por ultrapassar o limite de velocidade`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
