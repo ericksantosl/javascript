985659ff51becaa3111da0487ac3d2179ebbb905
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-02-12 09:47:54 -0300

    Update ambiente.js

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -4,3 +4,10 @@ num.sort()
 console.log(num)
 console.log(`Nosso vetor tem ${num.length} posições`)
 console.log(`O primeiro valor é ${num[0]}`)
+
+var pos = num.indexOf(3)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
