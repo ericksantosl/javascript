46cf32b58d17aef6168bb90d7a2038ca614559ea
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-02-12 08:50:43 -0300

    Update vetornatela.js

--- aulas/aula15/vetornatela.js
+++ aulas/aula15/vetornatela.js
@@ -1,7 +1,19 @@
 let num = [2, 4, 1, 5, 8]
 
+console.log(num)
+
+for(let pos in num) {
+    console.log(`O valor da posição ${pos} é ${num[pos]}`)
+}
+
+/*
+for(var pos = 0 ; pos < num.length ; pos++) {
+    console.log(`O valor da posição ${pos} é ${num[pos]}`)
+}
+
 console.log(num[0])
 console.log(num[1])
 console.log(num[2])
 console.log(num[3])
 console.log(num[4])
+*/
