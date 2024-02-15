daf302476b7d3d987f10e78f4d270362bd3efc9a
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-02-15 09:46:43 -0300

    Update script.js
    Resolvendo alguns bugs.

--- aulas/aula16ex/ex018/script.js
+++ aulas/aula16ex/ex018/script.js
@@ -19,33 +19,44 @@ function adicionar() {
         tab.appendChild(item)
     }
 
+    // O resultado do finalizar anterior é apagado quando adicionado outro valor
+    res.innerHTML = ''
+
 }
 
 function finalizar() {
     let res = window.document.querySelector('div#res')
 
-    // Colocando o array em ordem crescente
-    numeros.sort((a, b) => a - b)
+    if (numeros.length == 0) {
+        alert('Impossível calcular! Adicione valores antes de finalizar.')
+    } else {
+        // Impede que o usuário imprima vários resultados sequenciais
+        res.innerHTML = ''
 
-    // Maior número do vetor
-    maiorNumero = numeros[numeros.length - 1]
+        // Colocando o array em ordem crescente
+        numeros.sort((a, b) => a - b)
 
-    // Menor número do vetor
-    menorNumero = numeros[0]
+        // Maior número do vetor
+        maiorNumero = numeros[numeros.length - 1]
 
-    // Soma de todos os números do vetor
-    let soma = 0
-    for (let i = 0; i < numeros.length; i++) {
-        soma += numeros[i];
+        // Menor número do vetor
+        menorNumero = numeros[0]
+
+        // Soma de todos os números do vetor
+        let soma = 0
+        for (let i = 0; i < numeros.length; i++) {
+            soma += numeros[i];
+        }
+
+        // Media dos valores do vetor
+        let media = soma / numeros.length
+
+        // Imprimindo resultado na tela
+        res.innerHTML += `<p>Foram cadastrados ${numeros.length} números;</p>`
+        res.innerHTML += `<p>O maior número cadastrado foi: ${maiorNumero};</p>`
+        res.innerHTML += `<p>O menor número cadastrado foi: ${menorNumero};</p>`
+        res.innerHTML += `<p>Somando todos os valores, o resultado é: ${soma};</p>`
+        res.innerHTML += `<p>A média dos valores do vetor é: ${media}.</p>`
     }
 
-    // Media dos valores do vetor
-    let media = soma / numeros.length
-
-    // Imprimindo resultado na tela
-    res.innerHTML += `<p>Foram cadastrados ${numeros.length} números;</p>`
-    res.innerHTML += `<p>O maior número cadastrado foi: ${maiorNumero};</p>`
-    res.innerHTML += `<p>O menor número cadastrado foi: ${menorNumero};</p>`
-    res.innerHTML += `<p>Somando todos os valores, o resultado é: ${soma};</p>`
-    res.innerHTML += `<p>A média dos valores do vetor é: ${media}.</p>`
 }
