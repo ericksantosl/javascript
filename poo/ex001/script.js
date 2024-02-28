1852466d4a23e91004b7fbcd57eb56c4bbff1bc0
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-02-28 14:37:35 -0300

    Create script.js
    POO

---
+++ poo/ex001/script.js
@@ -0,0 +1,60 @@
+class Carro {
+    constructor (pnome, ptipo) {
+        this.nome = pnome
+        if (ptipo == 1) {
+            this.tipo = 'Popular'
+            this.vel = 160
+        } else if (ptipo == 2) {
+            this.tipo = 'Caminhonete'
+            this.vel = 180
+        } else if (ptipo == 3) {
+            this.tipo = 'Esportivo'
+            this.vel = 250
+        }
+    }
+
+    getNome() {
+        return this.nome
+    }
+
+    getTipo() {
+        return this.tipo
+    }
+
+    getVel() {
+        return this.vel
+    }
+
+    setNome(nome) {
+        this.nome = nome
+    } 
+
+    setTipo(tipo) {
+        this.tipo = tipo
+    }
+
+    setVel(vel) {
+        this.vel = vel
+    }
+
+    info() {
+        console.log(`Nome: ${this.nome}`)
+        console.log(`Tipo: ${this.tipo}`)
+        console.log(`Velocidade: ${this.vel}`)
+        console.log(`---------------`)
+    }
+}
+
+let c1 = new Carro('Gol', 1)
+let c2 = new Carro('Porshe', 3)
+let c3 = new Carro('S10', 2)
+
+c1.setNome('Palio')
+
+c1.info()
+//c2.info()
+//c3.info()
+
+
+
+
