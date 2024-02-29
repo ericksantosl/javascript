f4a917c2717c69e10e1c1facabd4e85807d98c9c
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-02-29 14:28:15 -0300

    Update pessoa.js

--- poo/ex004/pessoa.js
+++ poo/ex004/pessoa.js
@@ -1,58 +1,16 @@
-function Pessoa(pnome, pidade) {
-    this.nome = pnome,
-    
-    this.idade = pidade,
-
-    this.getNome = function() {
+const Pessoas = {
+    nome: 'Bruno',
+    getNome: function() {
         return this.nome
     },
-
-    this.getIdade = function() {
-        return this.idade
-    },
-
-    this.setNome = function(nome) {
+    setNome: function(nome) {
         this.nome = nome
-    },
-
-    this.setIdade = function(idade) {
-        this.idade = idade
-    },
-
-    this.info = function() {
-        console.log(`Nome: ${this.getNome}`)
-        console.log(`Idade: ${this.getIdade}`)
-        console.log(`-----------------`)
     }
 }
 
-let pessoas = []
+const p2 = Pessoas
 
-const res = document.querySelector('.res')
+p2.nome = 'Erick'
 
-function addPessoa() {
-    res.innerHTML = ''
-    pessoas.map((p) => {
-        const div = document.createElement("div")
-        div.setAttribute("class", "pessoa")
-        div.innerHTML = `Nome: ${p.getNome()}<br>Idade: ${p.getIdade()}<br>--------------------`
-        res.appendChild(div)
-    })
-}
-
-function adicionar() {
-    let nome = document.querySelector('#iname')
-
-    let idade = document.querySelector('#iidade')
-
-    let p = new Pessoa(nome.value, idade.value)
-
-    pessoas.push(p)
-
-    nome.value = ""
-    idade.value = ""
-    nome.focus()
-
-    addPessoa()
-
-}
+console.log(Pessoas.nome)
+console.log(p2.getNome)
