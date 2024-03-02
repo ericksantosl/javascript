089daa1dbb58efbd8fb5b045094439843b6b418b
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-03-02 15:05:54 -0300

    Update script.js

---
+++ poo/ex006/script.js
@@ -0,0 +1,23 @@
+let variavel = 100
+
+let arr = [100,200,300,400]
+
+let pessoa = ['Erick', 20, 'Desenvolvedor']
+
+let obj = {
+    nome: 'Erick',
+    idade: 20,
+    prof: 'Desenvolvedor',
+    carro: 'Porshe'
+
+}
+
+//console.log(variavel)
+
+//console.log(arr)
+
+//console.log(arr[3])
+
+//console.log(pessoa)
+
+console.log(obj.carro)
